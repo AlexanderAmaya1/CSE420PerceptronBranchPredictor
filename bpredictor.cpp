0d2e51d964dce308535451527653cc118ae672bc
Alex Amaya <52898195+AlexanderAmaya1@users.noreply.github.com>
2022-04-18 15:19:14 -0700

    perceptron w/ basic python script

--- bpredictor.cpp
+++ bpredictor.cpp
@@ -10,6 +10,8 @@
 #include <assert.h>
 #include <fstream> 
 #include <cmath>
+#include <vector>
+
 #include "pin.H"
 using namespace std;
 
@@ -18,7 +20,9 @@ static UINT64 takenIncorrect = 0;
 static UINT64 notTakenCorrect = 0;
 static UINT64 notTakenIncorrect = 0;
 
-
+ofstream outfile;
+UINT64 insCount = 0;
+UINT64 branchCount =0;
 
 class BranchPredictor {
 
@@ -136,30 +140,140 @@ struct correlatingPredictor{
 
 struct gshare{
   //Stores 12 bits of history information; all initially set to taken
-  UINT16 last_results = 4096-1;
+  UINT16 last_results = 8192-1;
 
-  threeBitPredictor threeList[4096];
+  threeBitPredictor threeList[8192];
 
 
   void update_prediction(bool taken_actually, UINT64 address){
 
     // Update prediction
-    threeList[(last_results) ^ (address%4096)].update_prediction(taken_actually);
+    threeList[(last_results) ^ (address%8192)].update_prediction(taken_actually);
     
     // Update last results 
     if(taken_actually){
-      last_results = ((last_results << 1) > (4096 -1))? ((last_results << 1) - 4096) + 1 : (last_results << 1) + 1;
+      last_results = ((last_results << 1) > (8192 -1))? ((last_results << 1) - 8192) + 1 : (last_results << 1) + 1;
     }else{
-      last_results = ((last_results << 1) > (4096-1))? (last_results << 1) - 4096: (last_results << 1);
+      last_results = ((last_results << 1) > (8192-1))? (last_results << 1) - 8192: (last_results << 1);
     }
   }
 
   bool get_prediction(UINT64 address){
-    return threeList[(last_results) ^ (address%4096)].get_prediction(address);
+    return threeList[(last_results) ^ (address%8192)].get_prediction(address);
 
   }
 
 };
+
+struct perceptron{
+
+ // 8192 rows of  weight registers 
+  vector<INT64> weight[8192];
+
+  // 8192 rows of n bit history registers 
+  vector<INT64> history[8192];
+
+  INT64 y[8192] = {0};
+
+  INT64 threshhold = (int)floor(1.93*256+14);
+
+  perceptron(){
+
+    for(int i = 0; i < 8192; i++){
+
+      for(int j = 0; j < 256; j++){
+
+        history[i].push_back(1);
+        weight[i].push_back(0);
+      }
+
+    }
+
+
+  }
+
+  void training(bool taken_actually, UINT64 address){
+      INT64 last_taken = 0;
+      if(taken_actually){
+        last_taken = 1;
+      }else{
+        last_taken = -1;
+      }
+
+    if( abs(y[address%8192]) <= threshhold || ((y[address%8192] >= 0) != taken_actually)){
+
+        for(int i  = 0; i < 256; i++){
+          
+          if(history[address % 8192][i] == last_taken){
+
+            weight[address % 8192][i]++;
+
+          }else{
+
+            weight[address % 8192][i]--;
+
+          }
+            
+
+        }
+
+    }
+
+
+
+
+  }
+
+  void update_prediction(bool taken_actually, UINT64 address){
+
+      INT64 last_taken = 0;
+
+      if(taken_actually){
+        last_taken = 1;
+      }else{
+        last_taken = -1;
+      }
+
+
+      //Update History
+      history[address % 8192].push_back(last_taken);
+      history[address % 8192].erase(history[address%8192].begin());
+      
+      weight[address % 8192].push_back(0);
+      weight[address % 8192].erase(weight[address%8192].begin());
+
+      //Train
+      training(taken_actually, address);
+
+      INT64 running = weight[address%8192][255];
+
+
+
+      for(UINT64 i = 1; i < 256; i++){
+
+        running += (weight[address%8192][i]*history[address%8192][i]);
+
+      }
+
+
+      y[address%8192] = running;
+
+
+
+
+  }
+
+  bool get_prediction(UINT64 address){
+
+   if(y[address%8192] >= 0)
+    return true;
+   else
+    return false;
+
+  }
+
+};
+
 
 struct tournament{
 
@@ -200,45 +314,13 @@ struct tournament{
 };
 
 
-struct perceptron_predictor{
-
-  UINT64 perceptron[512] = {0};
-  
-  UINT64 weight[512] = {0};
-
-  UINT64 history[1024] = {0};
-
-
-
-  void training(bool taken_actually){
-
-
-
-  }
-
-  void update_prediction(bool taken_actually, UINT64 address){
-
-
-
-  }
-
-  bool get_prediction(UINT64 address){
-
-
-
-  }
-
-};
-
-
-
 class myBranchPredictor: public BranchPredictor {
   public:
   myBranchPredictor() {
 
   }
 
-  tournament predictor;
+   tournament predictor;
 
   BOOL makePrediction(ADDRINT address){
 
@@ -260,13 +342,19 @@ myBranchPredictor* BP;
 // This knob sets the output file name
 KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "result.out", "specify the output file name");
 
+void branch_output(ADDRINT address, BOOL takenActually, BOOL takenPredicted ){
 
-// In examining handle branch, refer to quesiton 1 on the homework
+  outfile << insCount << "," << branchCount << "," <<address << "," << takenActually << "," << takenPredicted << "\n";
+ 
+}
+
+
 void handleBranch(ADDRINT ip, BOOL direction)
 {
-  
+
   BOOL prediction = BP->makePrediction(ip);
   BP->makeUpdate(direction, prediction, ip);
+  branchCount++;
 
   if(prediction && direction){
 
@@ -280,17 +368,28 @@ void handleBranch(ADDRINT ip, BOOL direction)
 
     takenIncorrect++;
 
+
   }else{
 
     notTakenCorrect++;
 
   }
 
+  branch_output(ip,direction, prediction);
+
 }
 
+VOID instrCount(UINT32 maxR){
+    
+    insCount++;
+
+}
 
 void instrumentBranch(INS ins, void * v)
-{   
+{ 
+
+  INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)instrCount, IARG_END);
+
   if(INS_IsBranch(ins) && INS_HasFallThrough(ins)) {
     INS_InsertCall(
       ins, IPOINT_TAKEN_BRANCH, (AFUNPTR)handleBranch,
@@ -307,35 +406,78 @@ void instrumentBranch(INS ins, void * v)
       IARG_END);
   }
 }
- 
+
+
+void ml_stats(ofstream& outfile){
+
+    double truePositives = (double)takenCorrect;
+    double falsePositives = (double)notTakenIncorrect;
+    double falseNegatives = (double)takenIncorrect;
+    double trueNegatives = (double)notTakenCorrect;
+
+    double accuracy = (truePositives + trueNegatives) / (truePositives + falseNegatives + falsePositives + trueNegatives);
+    double sensitivity = truePositives / (truePositives + falseNegatives);
+    double specifity = trueNegatives / (falsePositives + trueNegatives);
+    double truePositiveRate = sensitivity;
+    double falsePositiveRate = 1 - specifity;
+    double precision = truePositives / (truePositives + falsePositives);
+    double f1_score = 2 * ((precision * sensitivity) / (precision + sensitivity));
+
+    string roc_analysis = "";
+    if ((truePositiveRate == 1) && (falsePositiveRate == 0)) {
+        roc_analysis = "perfect";
+    }
+    else if (falsePositiveRate > truePositiveRate) {
+        roc_analysis = "worse";
+    }
+    else if (truePositiveRate > falsePositiveRate) {
+        roc_analysis = "better";
+    }
+    else if (truePositiveRate == falsePositiveRate) {
+        roc_analysis = "random";
+    }
+
+  outfile << "TP: " << truePositives << "\nFP: " << falsePositives << "\nTN: " << trueNegatives << "\nFN: " << falseNegatives << "\n\nACCURACY: " << accuracy << "\nSensitivity: " << sensitivity << "\nSpecifity: " << specifity << "\nPrecision: " << precision << "\nF1 Score: " << f1_score << "\n\nTrue Positive Rate: " << truePositiveRate << "\nFalse Positive Rate: " << falsePositiveRate << "\n\nROC: " << roc_analysis << "\n";
+
+}
 
 /* ===================================================================== */
 VOID Fini(int, VOID * v)
 { 
-  // double sum = (takenCorrect+takenIncorrect+notTakenCorrect+notTakenIncorrect);
-  // double sum_correct = takenCorrect+notTakenCorrect;
-  // double percent = sum_correct/sum * 100;
+  double sum = (takenCorrect+takenIncorrect+notTakenCorrect+notTakenIncorrect);
+  double sum_correct = takenCorrect+notTakenCorrect;
+  double percent = sum_correct/sum * 100;
   
-  //cout << "\nPercent Accuracy: " << percent <<endl;
+  cout << "\nPercent Accuracy: " << percent <<endl;
   //cout << "Size of Predictor: " << sizeof(BP->predictor) << " bytes out of 4125 bytes "<<endl;
 
 
-  ofstream outfile;
-  outfile.open(KnobOutputFile.Value().c_str());
-  outfile.setf(ios::showbase);
-  outfile << "takenCorrect: "<< takenCorrect <<"  takenIncorrect: "<< takenIncorrect <<" notTakenCorrect: "<< notTakenCorrect <<" notTakenIncorrect: "<< notTakenIncorrect <<"\n";
-  outfile.close();
+
+  //ofstream outfile;
+  ///outfile.open(KnobOutputFile.Value().c_str());
+ // outfile.setf(ios::showbase);
+  //outfile << "takenCorrect: "<< takenCorrect <<"  takenIncorrect: "<< takenIncorrect <<" notTakenCorrect: "<< notTakenCorrect <<" notTakenIncorrect: "<< notTakenIncorrect <<"\n";
+  
+  //ml_stats(outfile);
+
+  //outfile.close();
 }
+
 
 
 // argc, argv are the entire command line, including pin -t <toolname> -- ...
 int main(int argc, char * argv[])
 {
+
+
     // Make a new branch predictor
     BP = new myBranchPredictor();
 
     // Initialize pin
     PIN_Init(argc, argv);
+
+    outfile.open(KnobOutputFile.Value().c_str());
+    outfile.setf(ios::showbase);
 
     // Register Instruction to be called to instrument instructions
     INS_AddInstrumentFunction(instrumentBranch, 0);
@@ -346,6 +488,8 @@ int main(int argc, char * argv[])
     // Start the program, never returns
     PIN_StartProgram();
     
+    outfile.close();
+
     return 0;
 }
 
